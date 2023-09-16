b07e0bbeadeb7d24a97a44c29e36ac5f73cac2a4
Oluwatoyin <oluwatoyinjolaoso001@gmail>
2023-09-16 11:13:15 +0100

    Add css and logo set up

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    screens: {
+      sm: "374px",
+      // => @media (min-width: 576px) { ... }
+
+      md: "767px",
+      // => @media (min-width: 960px) { ... }
+
+      lg: "1023px",
+      // => @media (min-width: 1440px) { ... }
+      xl: "1200px",
+    },
+    extend: {
+      colors: {
+        "rose-700": "#BE123C",
+      },
+    },
   },
   plugins: [],
 };
