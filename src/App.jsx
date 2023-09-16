578518d18feb0d5a040a17771603132494e8898e
Oluwatoyin <oluwatoyinjolaoso001@gmail>
2023-09-16 11:22:17 +0100

    Implemented routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
+import { Link } from 'react-router-dom'
 import './App.css'
 
 function App() {
 
   return (
-  <div>Hello world</div>
+  <div>Hello world <Link to="/movie">Go to movie page</Link></div>
   )
 }
 
---
+++ src/components/Movie.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+
+const Movie = () => {
+  return (
+    <div>Movie</div>
+  )
+}
+
+export default Movie
